1b5ad00abbf61d9977195e750f9623f9bcabc83f
zxsye <61500799+zxsye@users.noreply.github.com>
2021-05-11 23:29:24 +1000

    asd

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 #include <stdlib.h> 
 #include <sys/types.h> 
 #include <sys/stat.h>
-#include <unistd.h>
+
 #include <sys/stat.h>
 #include <fcntl.h>
 
@@ -16,15 +16,15 @@
 int main(int argc, char** argv) {
     if((mkfifo(CHANNEL_NAME, S_IRWXU | S_IRWXG)) >= 0) { 
         int fd = open(CHANNEL_NAME, O_RDONLY); 
-        if(fd > 0) { 
-            FILE * read_channel = fdopen(fd, "r"); 
-            char buf[BUF_SZ]; 
-            while(fgets(buf, BUF_SZ, read_channel) != NULL) { 
-                puts(buf); 
-            }
-            printf("closing pipe...\n");
-            fclose(read_channel); 
-        }
+        // if(fd > 0) { 
+        //     FILE * read_channel = fdopen(fd, "r"); 
+        //     char buf[BUF_SZ]; 
+        //     while(fgets(buf, BUF_SZ, read_channel) != NULL) { 
+        //         puts(buf); 
+        //     }
+        //     printf("closing pipe...\n");
+        //     fclose(read_channel); 
+        // }
         close(fd);
     } else { 
         fprintf(stderr, "Unable to open pipe");
