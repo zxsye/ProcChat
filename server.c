e5b795a80d01668a60ee4a066f44cf3e46721ffe
zxsye <61500799+zxsye@users.noreply.github.com>
2021-05-15 02:55:06 +1000

    asdf

--- server.c
+++ server.c
@@ -434,7 +434,7 @@ int start_daemon(char * buffer) {
 
     // Open pipe as FD
     int fd_dae_WR = open(to_client_fp, O_NONBLOCK | O_RDWR);
-    int fd_dae_RD = open(to_daemon_fp, O_RDWR);
+    int fd_dae_RD = open(to_daemon_fp, O_NONBLOCK | O_RDWR);
     if (fd_dae_RD < 0 || fd_dae_WR < 0) {
 		perror("Failed to open gevent FD");
 		return 1;
