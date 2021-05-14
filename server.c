47c11659eab689605e4a63cb10693b2ebde4c714
zxsye <61500799+zxsye@users.noreply.github.com>
2021-05-14 15:17:23 +1000

    asdf

--- server.c
+++ server.c
@@ -90,6 +90,8 @@ int do_receive(char * buffer, const char * to_client_fp) {
     int fd = open(to_client_fp, O_NONBLOCK, O_WRONLY);
     if (write(fd, buffer, 2048) < 0) {
         fprintf(stderr, " %s ", to_client_fp);
+        fprintf(stderr, "Identifer: %s", buffer + 2);
+        fprintf(stderr, "Msg: %s", buffer + 2 + 256);
     } else {
         // DEBUG */ printf("wrote to client\n");
         // DEBUG */ printf("From: %s\nMsg: %s\n", buffer + 2, buffer + 2 + 256);
