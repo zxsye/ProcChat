6bf1d4d4baf8a4e348b1a9026108b54f4a73439f
zxsye <61500799+zxsye@users.noreply.github.com>
2021-05-16 21:39:27 +1000

    asdf

--- server.c
+++ server.c
@@ -399,7 +399,7 @@ int run_daemon(char * buffer) {
 		FD_SET(fd_dae_RD, &allfds); // 100000
 		
 		int select_ret = select(maxfd, &allfds, NULL, NULL, &timeout);
-        fprintf(stderr, "s: %ld || µs: %ld\n", timeout.tv_sec, timeout.tv_usec);
+        
         // ======= NEW UPDATE =======
 		if (select_ret < 0) {
             close(fd_dae_RD);
